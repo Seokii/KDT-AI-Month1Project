8b4587d52dafbddbc704520e189026fbdc1c44da
seokii <dev.seokii@gmail.com>
2022-10-14 01:55:13 +0900

    2022-10-14

--- board/models.py
+++ board/models.py
@@ -1,4 +1,7 @@
+import os.path
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,8 +9,21 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    head_image = models.ImageField(upload_to='board/images/%Y/%m/%d', blank=True)
+    file_upload = models.FileField(upload_to='board/files/%Y/%m/%d', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
+
+    def get_absolute_url(self):
+        return f'/board/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
