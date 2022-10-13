357fa9f592b0a7e391189f33cf993fea035f7732
seokii <dev.seokii@gmail.com>
2022-10-13 19:02:40 +0900

    make single_pages

--- board/views.py
+++ board/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
+class PostList(ListView):
+    model = Post
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'board/board_detail.html',
+        {
+            'post': post,
+        }
+    )
--- kdt_ai_month1/urls.py
+++ kdt_ai_month1/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
+    path('board', include('board.urls')),
+    path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
 ]
