3cbcffa046f14bca8427d88471b922c37bb16e60
seokii <dev.seokii@gmail.com>
2022-10-21 17:03:11 +0900

    settings DEBUG FALSE

--- kdt_ai_month1/settings.py
+++ kdt_ai_month1/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['kdt-month1.herokuapp.com',
                  '127.0.0.1'
